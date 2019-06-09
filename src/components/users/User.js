51bc9f10fe5841de457bd8f80ce7a5dd6bd49f43
Ryan Emslie <ryanemslie@Ryans-MacBook-Pro.local>
2019-06-09 12:56:01 -0400

    23_User Component UI and Layout

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import Spinner from "../layout/Spinner";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
     console.log("Loaded");
   }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+  };
 
   render() {
     const {
@@ -14,16 +23,86 @@ export class User extends Component {
       bio,
       blog,
       login,
-      htm_url,
+      html_url,
       followers,
       following,
       public_repos,
-      public_gist,
+      public_gists,
+      company,
       hireable
     } = this.props.user;
 
     const { loading } = this.props;
-    return <div>{name}</div>;
+    if (loading) return <Spinner />;
+
+    return (
+      <>
+        <Link to="/" className="btn btn-light">
+          Back to Search
+        </Link>
+        Hireable: {""}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-times-circle text-danger" />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              style={{ width: "150px" }}
+              alt=""
+            />
+            <h1>{name}</h1>
+            <p>{location}</p>
+          </div>
+          <div>
+            {bio && (
+              <>
+                <h3>Bio:</h3>
+                <p>{bio}</p>
+              </>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <>
+                    <strong>Username: </strong>
+                    {login}
+                  </>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <>
+                    <strong>Company: </strong>
+                    {company}
+                  </>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <>
+                    <strong>Website: </strong>
+                    {blog}
+                  </>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        </div>
+      </>
+    );
   }
 }
 

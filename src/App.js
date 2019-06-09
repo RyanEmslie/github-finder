088b41d71f87ea1e0cdc404de81e19c232ae700a
Ryan Emslie <ryanemslie@Ryans-MacBook-Pro.local>
2019-06-08 21:23:49 -0400

    20_Alert State and Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 import axios from "axios";
 
 import "./App.css";
@@ -9,7 +10,8 @@ import "./App.css";
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   // Search GitHub users
@@ -24,9 +26,15 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Clear user from State
   clearUsers = () => {
     console.log("I was pushed");
     this.setState({ users: [], loading: false });
+  };
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
   };
 
   render() {
@@ -35,10 +43,12 @@ class App extends Component {
       <div className="App">
         <Navbar icon="fab fa-github" title="Github - Finder" />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
